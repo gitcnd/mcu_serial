20dca0b8266a33a7cfaf129e0cb8f40dcfc0b4d1
Chris Drake <cnd@geek.net.au>
2023-11-28 19:19:29 +1000

    add exit on ^] and fix (remove) line buffering

--- mcu_serial.pl
+++ mcu_serial.pl
@@ -4,6 +4,7 @@ use warnings;
 use Device::SerialPort;
 use IO::Handle;
 use Getopt::Long;
+use Term::ReadKey;
 
 # Command-line options
 my $reset = 0;
@@ -14,7 +15,7 @@ my $port_name;
 GetOptions( "reset" => \$reset, "dfu"   => \$dfu, "exit"   => \$exit, "port=s" => \$port_name ) or die "Error in command line arguments\n";
 
 # Serial port configuration
-$port_name = '/dev/ttyS21' unless($port_name);
+$port_name = '/dev/ttyS25' unless($port_name);
 my $baud_rate = 115200;
 
 # Create and open the serial port
@@ -35,6 +36,8 @@ $port->rts_active(0);	# this is reset (setting this "high" resets the ESP32)
 STDOUT->autoflush(1);
 STDIN->autoflush(1);
 
+$port->datatype('raw');	# stop line buffering
+
 # Set STDIN to non-blocking mode
 my $old_fh = select(STDIN);
 $| = 1;
@@ -44,6 +47,8 @@ select($old_fh);
 &reset() if($reset);
 exit(0) if($exit);
 
+ReadMode('raw'); # Set terminal to raw mode to read characters immediately
+print "Use Control-] ( ^] ) to quit terminal emulator\n";
 # Main loop
 while (1) {
     # Create a read set for select
@@ -52,25 +57,33 @@ while (1) {
     vec($rin, $port->FILENO, 1) = 1;
 
     # Wait for data on serial port or STDIN
-    select($rin, undef, undef, undef);
+    select($rin, undef, undef, 0.1);
 
     # Check for data from the serial port
     if (vec($rin, $port->FILENO, 1)) {
-        my $data = $port->lookfor;
-        if ($data) {
-            print "Received: $data\n";
+        # my $data = $port->lookfor; # always line-buffered
+        my ($count, $data) = $port->read(255);  # Read up to 255 bytes
+        if ($count) {
+            #print "Received: $data\n";
+            print $data;
         }
     }
 
     # Check for data from STDIN (keyboard input)
     if (vec($rin, fileno(STDIN), 1)) {
         my $key = getc(STDIN);
+	last if defined $key && ord($key) == 29; # Exit on Ctrl+] 
         if (defined $key) {
             $port->write($key);
+            $port->write(chr(13)) if(ord($key)==10);
         }
     }
 }
 
+END {
+  ReadMode('restore'); # Restore terminal settings
+  print "\n";
+}
 
 sub reset {
   print "\nresetting...\n";
